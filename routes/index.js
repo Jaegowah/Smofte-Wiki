3b55adf9cd67d9e22bab2ace7c44889de5c7d7db
Karl Wolf <wolfkarl@gmail.com>
2015-03-17 14:53:30 +0100

    Parst und gibt statisch index.md aus

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var marked = require('marked');
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Smofte!' });
+	fs.readFile('data/index.md', 'utf8', function(err, data)
+	{
+  		var mdcontent = marked(data);
+  		console.log(req);
+  		res.render('index', { title: 'Smofte!', content: mdcontent });
+	});
 });
 
 module.exports = router;
