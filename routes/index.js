650156cc6961b94805b39382f3dcc9c1105eed99
Karl Wolf <wolfkarl@gmail.com>
2015-03-18 13:07:46 +0100

    Handle route using params vs. regex

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,28 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	
-	loadpage(res, 'data/index.md');
+	loadpage(res, 'data/index.md', next);
 });
 
-router.get('/*', function(req, res, next) {
-	path = "data" + req.path + ".md";
-	loadpage(res, path);
+router.get('/:page', function(req, res, next) {
+	path = "data/" + req.params.page + ".md";
+	loadpage(res, path, next);
 });
 
-function loadpage(res, path)
+function loadpage(res, path, next)
 {
 	fs.readFile(path, 'utf8', function(err, data)
 	{
+
+
 		console.log("Loading " + path);
+
+		if (err)
+			{
+				console.log("Error!")
+				return next
+			}
+
   		var mdcontent = marked(data);
   		// var title = "Seite ist " + req.path;
   		res.render('index', { title: "smofte", content: mdcontent });
