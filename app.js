fdc7f5b49aa84f40aad6cf470453ec6fc739913b
Karl Wolf <wolfkarl@gmail.com>
2015-03-27 15:43:22 +0100

    Kann Seiten erstellen und bearbeiten

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,94 @@ var router = express.Router();
 var marked = require('marked');
 var fs = require('fs');
 
+var pagepath = "data/";
+
+function path_to_page(pagename)
+{
+	return pagepath + pagename + ".md";
+}
+
+// alrighty someone tries to save their edits!
+router.post('/:page', function(req, res, next)
+{
+	var flash=""
+	console.log(req.headers);
+	if (req.hasOwnProperty("body"))
+	{
+		console.log("attempting to save...");
+		newcontent = req.body.md
+		path = path_to_page(req.params.page);
+		fs.writeFile(path, newcontent, function(err) {
+		    if(err) {
+		        return console.log(err);
+				flash = "Something went wrong saving: " + err; 
+		    }
+		    else
+		    {
+		    	console.log("The file was saved to "+path+"!");
+		    	flash = "Page save successfully.";
+		    }
+			
+			loadpage(res, req.params.page, next, flash);
+
+		}); 
+	}
+	else
+	{
+		flash = "wtf";
+		loadpage(res, req.params.page, next, flash);
+	}
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	
-	loadpage(res, 'data/index.md', next);
+	loadpage(res, 'index', next);
 });
 
 router.get('/:page', function(req, res, next) {
-	path = "data/" + req.params.page + ".md";
-	loadpage(res, path, next);
+
+
+	loadpage(res, req.params.page, next);
+
 });
 
-function loadpage(res, path, next)
+router.get('/:page/edit', function(req, res, next)
 {
+	console.log("Loading for Edit: " + path);
+	var path = path_to_page(req.params.page);
+	fs.readFile(path, 'utf8', function(err, data)
+	{
+		if (err)
+		{
+			// page doesn't exist, doesn't matter had sex 
+			next(req, res, next);
+			var mdcontent = ""
+		}
+		else
+		{
+  			var mdcontent = marked(data);
+		}
+
+  		// var title = "Seite ist " + req.path;
+  		res.render('edit', { pagename: req.params.page, rawmd: data });
+	});
+});
+
+
+
+function loadpage(res, pagename, next, flash)
+{
+	var path = path_to_page(pagename);
+
+	if (! /^[a-zA-Z0-9-_]+$/.test(pagename)) 
+	{
+    	// not a valid pagename
+    	console.log("not a page");
+    	return next();
+	}
+
 	fs.readFile(path, 'utf8', function(err, data)
 	{
 
@@ -25,13 +99,17 @@ function loadpage(res, path, next)
 
 		if (err)
 			{
-				console.log("Error!")
-				return next
+				// so the page is not existing
+				// but it is a valid page name, we've checked that
+				// so let's create a new page
+				res.render('new', {pagename: pagename});
 			}
-
-  		var mdcontent = marked(data);
-  		// var title = "Seite ist " + req.path;
-  		res.render('index', { title: "smofte", content: mdcontent });
+		else
+		{
+			var mdcontent = marked(data);
+	  		// var title = "Seite ist " + req.path;
+	  		res.render('index', { pagename: pagename, content: mdcontent, flash: flash});
+		}
 	});
 }
 
